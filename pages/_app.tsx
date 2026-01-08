0fd3bb4b0ef9e712a01b26ffc1c0faba8d7ab19e
edoardo-hash <edoardo@inveniohomes.com>
2026-01-08 21:55:51 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,9 +29,28 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
 
+      {/* Meta Pixel */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1233510735344879');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
       {/* Main content */}
       <div className="pb-24">
-        {/* padding so WhatsApp button doesn’t overlap footer */}
         <Component {...pageProps} />
       </div>
 
